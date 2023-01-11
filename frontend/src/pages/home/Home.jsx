ea9fae5cc86e77a41bf66d960a12284471ce2fb2
mohamedInfath <mohamedinfath99@gmail.com>
2023-01-11 11:17:15 +0530

    modified

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -18,10 +18,10 @@ function home() {
       <div className="homeContainer">
         <Featured />
         
-        <h1 className='homeTitle'>Browse by property type</h1>
+        <h1 className='homeTitle'>Browse by property type : </h1>
         <PropertyList />
 
-        <h1 className='homeTitle'>Homes guests love</h1>
+        <h1 className='homeTitle'>Homes guests love : </h1>
         <FeaturedProperties />
         <MailSection />
         <Footer />
