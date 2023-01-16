dcccb7f61d055aab3a4da0ea79e74fbc27cd395f
mohamedInfath <mohamedinfath99@gmail.com>
2023-01-16 23:36:43 +0530

    file modified

--- admin/src/formSource.js
+++ admin/src/formSource.js
@@ -1,40 +1,49 @@
 export const userInputs = [
   {
-    id: "username",
+    id: 1,
     label: "Username",
     type: "text",
     placeholder: "john_doe",
   },
   {
-    id: "email",
+    id: 2,
+    label: "Name and surname",
+    type: "text",
+    placeholder: "John Doe",
+  },
+  {
+    id: 3,
     label: "Email",
-    type: "email",
+    type: "mail",
     placeholder: "john_doe@gmail.com",
   },
   {
-    id: "phone",
+    id: 4,
     label: "Phone",
     type: "text",
     placeholder: "+1 234 567 89",
   },
   {
-    id: "password",
+    id: 5,
     label: "Password",
     type: "password",
   },
   {
-    id: "country",
+    id: 6,
+    label: "Address",
+    type: "text",
+    placeholder: "Elton St. 216 NewYork",
+  },
+  {
+    id: 7,
     label: "Country",
     type: "text",
     placeholder: "USA",
   },
-  {
-    id: "city",
-    label: "City",
-    type: "text",
-    placeholder: "USA",
-  },
 ];
+
+
+
 
 export const productInputs = [
   {
@@ -66,83 +75,5 @@ export const productInputs = [
     label: "Stock",
     type: "text",
     placeholder: "in stock",
-  },
-];
-
-export const hotelInputs = [
-  {
-    id: "name",
-    label: "Name",
-    type: "text",
-    placeholder: "My Hotel",
-  },
-  {
-    id: "type",
-    label: "Type",
-    type: "text",
-    placeholder: "hotel",
-  },
-  {
-    id: "city",
-    label: "City",
-    type: "text",
-    placeholder: "New York",
-  },
-  {
-    id: "address",
-    label: "Address",
-    type: "text",
-    placeholder: "elton st, 216",
-  },
-  {
-    id: "distance",
-    label: "Distance from City Center",
-    type: "text",
-    placeholder: "500",
-  },
-  {
-    id: "title",
-    label: "Title",
-    type: "text",
-    placeholder: "The best Hotel",
-  },
-  {
-    id: "desc",
-    label: "Description",
-    type: "text",
-    placeholder: "description",
-  },
-  {
-    id: "cheapestPrice",
-    label: "Price",
-    type: "text",
-    placeholder: "100",
-  },
-];
-
-export const roomInputs = [
-  {
-    id: "title",
-    label: "Title",
-    type: "text",
-    placeholder: "2 bed room",
-  },
-  {
-    id: "desc",
-    label: "Description",
-    type: "text",
-    placeholder: "King size bed, 1 bathroom",
-  },
-  {
-    id: "price",
-    label: "Price",
-    type: "number",
-    placeholder: "100",
-  },
-  {
-    id: "maxPeople",
-    label: "Max People",
-    type: "number",
-    placeholder: "2",
   },
 ];
