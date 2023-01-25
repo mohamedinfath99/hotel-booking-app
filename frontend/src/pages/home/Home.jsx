bfc0a6ff88490c1bd294faa5ec2f596b6a7a682f
mohamedInfath <mohamedinfath99@gmail.com>
2023-01-25 10:20:09 +0530

    add new features

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -18,7 +18,7 @@ function home() {
       <div className="homeContainer">
         <Featured />
         
-        <h1 className='homeTitle'>Browse by property type : </h1>
+        <h1 className='homeTitle'>Browse by place type : </h1>
         <PropertyList />
 
         <h1 className='homeTitle'>Homes guests love : </h1>
